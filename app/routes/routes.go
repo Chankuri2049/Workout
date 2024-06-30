9ee7f2788a1b7a0579928caaf9e0f6a9f3878386
Chankuri2049 <godchestnut1208@gmail.com>
2024-06-30 14:57:03 +0900

    add: GraphQL設定修正

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,10 +1,28 @@
 package routes
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	graph "github.com/Chankuri2049/Workout/graphql"
+	"github.com/Chankuri2049/Workout/graphql/resolver"
 	"github.com/labstack/echo/v4"
 )
 
-func RegisterRoutes(server *echo.Echo) {
-	server.GET("/user", getAllUsers)
-	server.GET("/user/:userId", getUserById)
+func RegisterRoutes(e *echo.Echo) {
+	graphqlHandler := handler.NewDefaultServer(
+		graph.NewExecutableSchema(
+			graph.Config{Resolvers: &resolver.Resolver{}},
+		),
+	)
+	playgroundHandler := playground.Handler("GraphQL", "/query")
+
+	e.POST("/query", func(c echo.Context) error {
+		graphqlHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	e.GET("/playground", func(c echo.Context) error {
+		playgroundHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
 }
--- app/server.go
+++ app/server.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"log"
-	"net/http"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/Chankuri2049/Workout/db"
-	"github.com/Chankuri2049/Workout/graph"
+	"github.com/Chankuri2049/Workout/routes"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -20,33 +17,10 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", welcome())
-
-	graphqlHandler := handler.NewDefaultServer(
-		graph.NewExecutableSchema(
-			graph.Config{Resolvers: &graph.Resolver{}},
-		),
-	)
-	playgroundHandler := playground.Handler("GraphQL", "/query")
-
-	e.POST("/query", func(c echo.Context) error {
-		graphqlHandler.ServeHTTP(c.Response(), c.Request())
-		return nil
-	})
-
-	e.GET("/playground", func(c echo.Context) error {
-		playgroundHandler.ServeHTTP(c.Response(), c.Request())
-		return nil
-	})
+	routes.RegisterRoutes(e)
 
 	err := e.Start(":5050")
 	if err != nil {
 		log.Fatalln(err)
-	}
-}
-
-func welcome() echo.HandlerFunc {
-	return func(c echo.Context) error {
-		return c.String(http.StatusOK, "Welcome!")
 	}
 }
